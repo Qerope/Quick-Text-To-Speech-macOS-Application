c10b464ad86744a5208b0e9cdbcbfe536cb80cdf
Hamed Tavakoli <preloopadm@gmail.com>
2021-01-04 16:44:09 +0330

    Initial Commit

---
+++ Q TTS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Q TTS
+//
+//  Created by Qerope Santos on 1/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
